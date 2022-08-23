b74fff75eeac5acd7b63d2e5a9c027474e2938a8
justbegginer <blackcloudsinthesky@protonmail.com>
2022-08-23 18:40:38 +0300

    add functions to repo

--- src/main/java/org/student/site/StudentsAndGroupBoot/repo/StudentRepo.java
+++ src/main/java/org/student/site/StudentsAndGroupBoot/repo/StudentRepo.java
@@ -3,6 +3,11 @@ package org.student.site.StudentsAndGroupBoot.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.student.site.StudentsAndGroupBoot.models.Student;
 
+import java.util.List;
+
 
 public interface StudentRepo extends JpaRepository<Student, Integer> {
+    List<Student> findStudentByName(String searchWord);
+    List<Student> findStudentBySurname(String searchWorld);
+    List<Student> findStudentByGroupNumber(Integer id);
 }
--- src/main/java/org/student/site/StudentsAndGroupBoot/repo/TutorRepo.java
+++ src/main/java/org/student/site/StudentsAndGroupBoot/repo/TutorRepo.java
@@ -3,6 +3,11 @@ package org.student.site.StudentsAndGroupBoot.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.student.site.StudentsAndGroupBoot.models.Tutor;
 
+import java.util.List;
+
 
 public interface TutorRepo extends JpaRepository<Tutor, Integer> {
+    List<Tutor> findTutorByName(String searchWord);
+    List<Tutor> findTutorBySurname(String searchWorld);
+    List<Tutor> findTutorByQualification(String searchWorld);
 }
