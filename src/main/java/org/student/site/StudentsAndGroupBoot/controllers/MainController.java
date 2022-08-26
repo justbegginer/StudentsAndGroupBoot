5c34452586c393b8371c7d5648c9285cd5b490c9
justbegginer <blackcloudsinthesky@protonmail.com>
2022-08-26 03:09:45 +0300

    refactoring

--- src/main/java/org/student/site/StudentsAndGroupBoot/controllers/MainController.java
+++ src/main/java/org/student/site/StudentsAndGroupBoot/controllers/MainController.java
@@ -29,16 +29,13 @@ public class MainController {
 
     @GetMapping("/search")
     public String search(Model model, @RequestParam(value = "word", required = false) String word) {
-        System.out.println("working");
         try {
-            System.out.println("id " + word + "?");
             int id = Integer.parseInt(word);
             model.addAttribute("groups", groupRepo.findById(id).get());
             model.addAttribute("students", studentRepo.findById(id).get());
             model.addAttribute("tutors", tutorRepo.findById(id).get());
             model.addAttribute("word", id);
         } catch (NumberFormatException exception) {
-            System.out.println("catching word");
             List<Student> studentList = studentRepo.findStudentByName(word);
             studentList.addAll(studentRepo.findStudentBySurname(word));
             model.addAttribute("students", studentList);
