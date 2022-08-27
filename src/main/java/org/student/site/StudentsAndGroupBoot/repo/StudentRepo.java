6cba62f7dd7ebfffcb707ec11387aca1b45e0a7b
justbegginer <blackcloudsinthesky@protonmail.com>
2022-08-28 02:06:28 +0300

    add function what can search by containing search word in name or surname of student

--- src/main/java/org/student/site/StudentsAndGroupBoot/repo/StudentRepo.java
+++ src/main/java/org/student/site/StudentsAndGroupBoot/repo/StudentRepo.java
@@ -1,6 +1,7 @@
 package org.student.site.StudentsAndGroupBoot.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.student.site.StudentsAndGroupBoot.models.Student;
 
 import java.util.List;
@@ -12,4 +13,9 @@ public interface StudentRepo extends JpaRepository<Student, Integer> {
     List<Student> findStudentBySurname(String searchWorld);
 
     List<Student> findStudentByGroupNumber(Integer id);
+    @Query("select student from Student student where student.name like concat('%', ?1, '%')")
+    List<Student> findStudentByIncludingInName(String searchWorld);
+
+    @Query("select student from Student student where student.surname like concat('%', ?1, '%')")
+    List<Student> findStudentByIncludingInSurname(String searchWorld);
 }
