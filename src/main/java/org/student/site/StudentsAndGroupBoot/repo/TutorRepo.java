c0ce28f58d2ae155b8484a2b10b0dd254bfdf824
justbegginer <blackcloudsinthesky@protonmail.com>
2022-08-28 02:07:05 +0300

    add functions what can search by containing search word in name,surname or qualification of tutor

--- src/main/java/org/student/site/StudentsAndGroupBoot/repo/TutorRepo.java
+++ src/main/java/org/student/site/StudentsAndGroupBoot/repo/TutorRepo.java
@@ -1,6 +1,8 @@
 package org.student.site.StudentsAndGroupBoot.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.student.site.StudentsAndGroupBoot.models.Student;
 import org.student.site.StudentsAndGroupBoot.models.Tutor;
 
 import java.util.List;
@@ -12,4 +14,13 @@ public interface TutorRepo extends JpaRepository<Tutor, Integer> {
     List<Tutor> findTutorBySurname(String searchWorld);
 
     List<Tutor> findTutorByQualification(String searchWorld);
+
+    @Query("select tutor from Tutor tutor where tutor.name like concat('%', ?1, '%')")
+    List<Tutor> findTutorByIncludingInName(String searchWorld);
+
+    @Query("select tutor from Tutor tutor where tutor.surname like concat('%', ?1, '%')")
+    List<Tutor> findTutorByIncludingInSurname(String searchWorld);
+
+    @Query("select tutor from Tutor tutor where tutor.qualification like concat('%', ?1, '%')")
+    List<Tutor> findTutorByIncludingInQualification(String searchWorld);
 }
