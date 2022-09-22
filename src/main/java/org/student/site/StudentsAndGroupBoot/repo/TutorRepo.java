07a5d7c3ca0d1fc19a678194465347c8e47fa5f4
justbegginer <blackcloudsinthesky@protonmail.com>
2022-09-23 02:27:24 +0300

    add searching method in repo

--- src/main/java/org/student/site/StudentsAndGroupBoot/repo/TutorRepo.java
+++ src/main/java/org/student/site/StudentsAndGroupBoot/repo/TutorRepo.java
@@ -3,7 +3,6 @@ package org.student.site.StudentsAndGroupBoot.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.student.site.StudentsAndGroupBoot.models.Student;
 import org.student.site.StudentsAndGroupBoot.models.Tutor;
 
 import java.util.List;
@@ -24,4 +23,13 @@ public interface TutorRepo extends JpaRepository<Tutor, Integer> {
 
     @Query("select tutor from Tutor tutor where tutor.qualification like concat('%', ?1, '%')")
     List<Tutor> findTutorByIncludingInQualification(String searchWorld);
+
+    @Query("select tutor from Tutor tutor " +
+            "where (tutor.name like concat('%', ?1, '%') and  tutor.surname like concat('%', ?2, '%') and tutor.qualification like concat('%', ?3, '%'))" +
+            "or (tutor.name like concat('%', ?1, '%') and  tutor.surname like concat('%', ?3, '%') and tutor.qualification like concat('%', ?2, '%')) " +
+            "or (tutor.name like concat('%', ?2, '%') and  tutor.surname like concat('%', ?1, '%') and tutor.qualification like concat('%', ?3, '%')) " +
+            "or (tutor.name like concat('%', ?2, '%') and  tutor.surname like concat('%', ?3, '%') and tutor.qualification like concat('%', ?1, '%')) " +
+            "or (tutor.name like concat('%', ?3, '%') and  tutor.surname like concat('%', ?1, '%') and tutor.qualification like concat('%', ?2, '%')) " +
+            "or (tutor.name like concat('%', ?3, '%') and  tutor.surname like concat('%', ?2, '%') and tutor.qualification like concat('%', ?1, '%'))")
+    List<Tutor> findTutorByIncludingInNameSurnameAndQualification(String searchWorld1, String searchWorld2, String searchWorld3);
 }
