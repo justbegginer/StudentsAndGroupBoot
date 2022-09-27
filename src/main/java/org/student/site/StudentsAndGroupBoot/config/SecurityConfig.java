ba865e8d71644d5210568b0985e1cd6b8b69d55d
justbegginer <blackcloudsinthesky@protonmail.com>
2022-09-27 17:58:20 +0300

    add roles from users table

--- src/main/java/org/student/site/StudentsAndGroupBoot/config/SecurityConfig.java
+++ src/main/java/org/student/site/StudentsAndGroupBoot/config/SecurityConfig.java
@@ -8,27 +8,29 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.student.site.StudentsAndGroupBoot.models.Role;
+import org.student.site.StudentsAndGroupBoot.services.impl.UserServiceImpl;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserServiceImpl userService;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/")
-                .permitAll()
-                .antMatchers("/groups")
-                .permitAll()
-                .antMatchers("/students")
-                .permitAll()
-                .antMatchers("/tutors")
+                .antMatchers("/", "/groups", "/students", "/tutors")
                 .permitAll()
                 .antMatchers(HttpMethod.GET, "/groups/**", "/students/**", "/tutors/**").hasAnyRole(Role.ADMIN.name(), Role.USER.name())
                 .antMatchers(HttpMethod.POST, "/groups/**", "/students/**", "/tutors/**").hasRole(Role.ADMIN.name())
@@ -41,17 +43,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     @Override
     protected UserDetailsService userDetailsService() {
+        List<org.student.site.StudentsAndGroupBoot.models.User> userList = userService.findAll();
+        List<UserDetails> springSecurityUserList = new ArrayList<>(userList.size());
+        for (int i = 0; i < userList.size(); i++) {
+            springSecurityUserList.add(User.builder()
+                    .username(userList.get(i).getEmail())
+                    .password(passwordEncoder().encode(userList.get(i).getPassword()))
+                    .roles(userList.get(i).getRole() + userList.get(i).getId())
+                    .build());
+        }
+        springSecurityUserList.add(User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles(Role.ADMIN.name())
+                .build());
+        springSecurityUserList.add(User.builder()
+                .username("username")
+                .password(passwordEncoder().encode("username"))
+                .roles(Role.USER.name())
+                .build());
         return new InMemoryUserDetailsManager(
-                User.builder()
-                        .username("admin")
-                        .password(passwordEncoder().encode("admin"))
-                        .roles(Role.ADMIN.name())
-                        .build(),
-                User.builder()
-                        .username("username")
-                        .password(passwordEncoder().encode("username"))
-                        .roles(Role.USER.name())
-                        .build()
+                springSecurityUserList
         );
     }
     @Bean
