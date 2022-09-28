0adefb8846709153f158b09c4d9346d9a2f83dff
justbegginer <blackcloudsinthesky@protonmail.com>
2022-09-29 02:52:20 +0300

    remake user

--- src/main/java/org/student/site/StudentsAndGroupBoot/models/User.java
+++ src/main/java/org/student/site/StudentsAndGroupBoot/models/User.java
@@ -17,27 +17,35 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @NotEmpty(message = "user's role shouldn't be empty")
-    @Size(min = 2, max = 30, message = "user's role should be between 2 and 30")
-    @Pattern(regexp = "^[a-z]*$",message = "user's role should contains only letters")
+    //@NotEmpty(message = "user's role shouldn't be empty")
+    //@Size(min = 2, max = 30, message = "user's role should be between 2 and 30")
+    //@Pattern(regexp = "^[a-z]*$",message = "user's role should contains only letters")
     private String role;
 
-    @NotEmpty(message = "user's type shouldn't be empty")
-    @Size(min = 2, max = 30, message = "user's type should be between 2 and 30")
-    @Pattern(regexp = "^[a-z]*$",message = "user's type should contains only letters")
+    //@NotEmpty(message = "user's type shouldn't be empty")
+    //@Size(min = 2, max = 30, message = "user's type should be between 2 and 30")
+    //@Pattern(regexp = "^[a-z]*$",message = "user's type should contains only letters")
     private String login;
 
-    @Email
-    @NotEmpty(message = "user's email shouldn't be empty")
-    @Size(min = 2, max = 30, message = "user's email should be between 2 and 30")
-    @Pattern(regexp = "^[a-z]*$",message = "user's email should contains only letters")
+    //@Email
+    //@NotEmpty(message = "user's email shouldn't be empty")
+    //@Size(min = 2, max = 30, message = "user's email should be between 2 and 30")
+    //@Pattern(regexp = "^[a-z]*$",message = "user's email should contains only letters")
     private String email;
 
-    @NotEmpty(message = "user's password shouldn't be empty")
-    @Size(min = 2, max = 30, message = "user's password should be between 2 and 30")
-    @Pattern(regexp = "^[a-z]*$",message = "user's password should contains only letters")
+    //@NotEmpty(message = "user's password shouldn't be empty")
+    //@Size(min = 2, max = 30, message = "user's password should be between 2 and 30")
+    //@Pattern(regexp = "^[a-z]*$",message = "user's password should contains only letters")
     private String password;
 
-    @Min(value = 1, message = "user's id should be more than 1")
+    //@Min(value = 1, message = "user's id should be more than 1")
     private int userId;
+
+
+    public void setLoginBasedOnEmail(){
+        if (this.email == null){
+            return;
+        }
+        this.login = this.email.split("@")[0];
+    }
 }
