c795ccb29d5b251946f6093d9cac53cb0eb8590f
justbegginer <blackcloudsinthesky@protonmail.com>
2023-06-22 04:19:08 +0300

    add controller for user

---
+++ src/main/java/org/student/site/StudentsAndGroupBoot/controllers/UserRestController.java
@@ -0,0 +1,54 @@
+package org.student.site.StudentsAndGroupBoot.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.student.site.StudentsAndGroupBoot.dto.PasswordsPair;
+import org.student.site.StudentsAndGroupBoot.exceptions.IncorrectDataException;
+import org.student.site.StudentsAndGroupBoot.exceptions.NotFoundException;
+import org.student.site.StudentsAndGroupBoot.models.Status;
+import org.student.site.StudentsAndGroupBoot.models.StatusPattern;
+import org.student.site.StudentsAndGroupBoot.models.User;
+import org.student.site.StudentsAndGroupBoot.services.impl.UserServiceImpl;
+
+import java.util.List;
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/rest/users")
+public class UserRestController {
+
+    private final UserServiceImpl userService;
+
+    public UserRestController(@Autowired UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        if (userService.findAll().isEmpty()){
+            throw new NotFoundException("There is no user");
+        }
+        return userService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable("id") Integer id) {
+        if (userService.findById(id).isEmpty()){
+            throw new NotFoundException("There is no user with id " + id);
+        }
+        return userService.findById(id).get();
+    }
+    @PatchMapping("/{type}/{id}")
+    public Status changePassword(@RequestBody PasswordsPair passwordsPair,
+                                 @PathVariable("type") String type,
+                                 @PathVariable("id") Integer id){
+        if (!Objects.equals(userService.findTopByRoleAndUserId(type, id).get().getPassword(), passwordsPair.getOldPassword())) {
+            throw new IncorrectDataException("Old password incorrect");
+        }
+        User user = userService.findTopByRoleAndUserId(type, id).get();
+        user.setPassword(passwordsPair.getNewPassword());
+        userService.save(user);
+        return new Status(true, StatusPattern.SUCCESS, null);
+    }
+
+}
